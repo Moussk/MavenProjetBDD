c60df5100776217abe0734f2430511ff130e2644
moussk <babakanoute@gmail.com>
2019-01-06 18:59:06 +0100

    ajout de code dans la classe de test crée

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import cucumber.annotation.en.When;
+import cucumber.runtime.PendingException;
 
 public class MyStepdefs {
+    @When("^the search phrase \"([^\"]*)\" is entered$")
+    public void theSearchPhraseIsEntered(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+        System.out.println("Voici le resultat");
+        throw new PendingException();
+    }
 }
